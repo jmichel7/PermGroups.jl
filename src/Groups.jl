e7295a993dfd46af17bc12866eb931811cd1f228
Jean Michel <jean.michel@imj-prg.fr>
2022-10-26 17:39:29 +0200

    added minimal_word(G,w)

--- src/Groups.jl
+++ src/Groups.jl
@@ -264,8 +264,9 @@ end
 `words_transversal(gens,p,action::Function=^)`
 
 A   transversal   recording   words.   returns   a  `Dict`  `t`  with  keys
-`orbit(G,p,action)`  and where `t[x]`  is a sequence  of integers such that
-`x==action(p,G(t[x]...))`
+`orbit(gens,p,action)` and where `t[x]` is a sequence of integers such that
+`x==action(p,prod(gens[t[x]]))`,  that is for each element `x` of the orbit
+of `p` describes as a word in `gens` an element bringing `p` to `x`.
 
 ```julia-repl
 julia> words_transversal([Perm(1,2),Perm(2,3)],1)
@@ -408,6 +409,34 @@ function minimal_words(G::Group{T})where T
   get!(G,:minwords)do
     words_transversal(gens(G),one(G),(x,y)->x*y)
   end::Dict{T,Vector{Int}}
+end
+
+"""
+`minimal_words(G::Group,w)`
+
+Gives all expressions of `w` as words in the generators of `G` of
+minimal length (uses `minimal_words(G)`).
+```julia-repl
+julia> G=Group(Perm(1,2),Perm(2,3));
+
+julia> minimal_words(G,Perm(1,3))
+2-element Vector{Vector{Int64}}:
+ [1, 2, 1]
+ [2, 1, 2]
+```
+"""
+function minimal_words(G::Group,w)
+  d=minimal_words(G)
+  m=d[w]
+  if length(m)<=1 return [m] end
+  res=Vector{Int}[]
+  for i in eachindex(gens(G))
+    v=w*G(-i)
+    if length(d[v])<length(m) 
+      append!(res,push!.(copy.(minimal_words(G,v)),i)) 
+    end
+  end
+  res
 end
 
 """
