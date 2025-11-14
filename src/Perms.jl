5b8b71bb1a162272f4bc2bbcbf08f2cff5fdfb5f
Jean Michel <jean.michel@imj-prg.fr>
2025-11-14 17:23:55 +0100

    doc fixes

--- src/Perms.jl
+++ src/Perms.jl
@@ -1,17 +1,17 @@
 """
-This package implements permutations and some functions of them. It depends
-only  on  the  package  `Combinat`.
+This  module implements  permutations and  associated functions. It depends
+only on the package `Combinat`.
 
-This  package  follows  the  design  of  permutations  in the GAP language.
-[`Perm`](@ref)s  are permutations of the  set `1:n`, represented internally
-as a vector of `n` integers holding the images of `1:n`. The integer `n` is
-called  the degree  of the  permutation. In  this package,  as in  GAP (and
-contrary  to the philosophy of Magma or the package `Permutations.jl`), two
-permutations  of different  degrees can  be multiplied  (the result has the
-larger  degree). Two permutations  are equal if  and only if  they move the
-same points in the same way, so two permutations of different degree can be
-equal; the degree is thus an implementation detail so usually it should not
-be used. One should rather use the function [`last_moved`](@ref).
+We  follow the design of permutations  in the GAP language. [`Perm`](@ref)s
+are  permutations of the  set `1:n`, represented  internally as a vector of
+`n`  integers holding the  images of `1:n`.  The integer `n`  is called the
+degree  of the permutation. In this module,  as in GAP (and contrary to the
+philosophy  of Magma or the package `Permutations.jl`), two permutations of
+different degrees can be multiplied (the result has the larger degree). Two
+permutations are equal if and only if they move the same points in the same
+way,  so two permutations of  different degree can be  equal; the degree is
+thus  an implementation detail so usually it should not be used. One should
+rather use the function [`last_moved`](@ref).
 
 This  design makes it  easy to multiply  permutations coming from different
 groups, like a group and one of its subgroups. It has a negligible overhead
@@ -21,15 +21,15 @@ The default constructor for a permutation uses the list of images of `1:n`,
 like  `Perm([2,3,1,5,4,6])`.  Often  it  is  more  convenient  to use cycle
 decompositions:    the   above   permutation    has   cycle   decomposition
 `(1,2,3)(4,5)`    thus   can   be    written   `Perm(1,2,3)*Perm(4,5)`   or
-`perm"(1,2,3)(4,5)"`  (this last form  can parse a  permutation coming from
-GAP  or the default printing at the REPL).  The list of images of `1:n` can
-be recovered from the permutation by the function [`perm`](@ref); note that
-equal  permutations with different degrees will have different `perm`. Note
-that the default constructor tests the validity of the input by calling the
-`julia` function `isperm`. To have a faster constructor which does not test
-the input, use the keyword argument `check=false`.
+`perm"(1,2,3)(4,5)"`;  this last form  can parse a  permutation coming from
+GAP or the default printing at the REPL. The list of images of `1:n` can be
+recovered  from the permutation  by the function  [`perm`](@ref); note that
+equal  permutations with different degrees  will have different `perm`. The
+default  constructor tests the validity of the input by calling the `julia`
+function  `isperm`. To  have a  faster constructor  which does not test the
+input, use the keyword argument `check=false`.
 
-The  complete type of a permutation  is `Perm{T}` where `T<:Integer`, where
+The complete type of a permutation is `Perm{T}` where `T<:Integer`, so that
 `Vector{T}`  is the type of the vector which holds the image of `1:n`. This
 can be used to save space or time. For instance `Perm{UInt8}(1,2,3)` can be
 used for Weyl groups of rank≤8 since they permute at most 240 roots. If `T`
@@ -117,7 +117,7 @@ Other  methods on  permutations are  [`cycles`](@ref), [`cycletype`](@ref),
 [`support`](@ref),        [`sortPerm`](@ref),        [`Perm_rowcol`](@ref),
 [`preimage`](@ref), [`randPerm`](@ref).
 
-No  method is given in  this package to enumerate  `Perm`s; you can use the
+No  method is given in  this module to enumerate  `Perm`s; you can use the
 method  `permutations` from `Combinat`,  iterate `Combinat.Permutations` or
 iterate the elements of `symmetric_group` from `PermGroups`.
 """
